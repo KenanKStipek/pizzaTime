077aad09102d225842d3a4f429014732e3018adc
Kenan Stipek <kenan@stipek.org>
2024-10-24 18:19:26 -0700

    fix file name and extension

--- src/api/main.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-
-import app from './controllers/orders'
-
-if(process.env.ENV === 'dev') {
-  app.listen(3000, () => console.log("Server ready on port 3000."));
-} else {
-  module.exports = app;
-}
